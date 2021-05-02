706a758733282fd269449656d53cab7a25a200f2
Georges034302 <georges.boughantous@gmail.com>
2021-05-02 09:39:44 +0100

    lab10

---
+++ lab10-demo/src/StadiumApplication.java
@@ -0,0 +1,19 @@
+
+import javafx.application.*;
+import javafx.stage.*;
+import au.edu.uts.ap.javafx.*;
+import static javafx.application.Application.launch;
+import model.*;
+
+//public class StadiumApplication extends Application {
+public class StadiumApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        ViewLoader.showStage(new Stadium(), "/view/stadium.fxml", "Stadium", stage);
+    }
+}
---
+++ lab10-demo/src/controller/StadiumController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.fxml.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import javafx.beans.property.*;
+import java.io.*;
+import au.edu.uts.ap.javafx.*;
+import javafx.scene.input.MouseEvent;
+import model.*;
+
+public class StadiumController extends Controller<Stadium> {
+
+}
